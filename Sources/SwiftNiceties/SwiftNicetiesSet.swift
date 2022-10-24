b9d19707198c535ab5fb7a34b57b4ca8e9716efe
Synapticloop <synapticloop@users.noreply.github.com>
2022-10-25 09:06:48 +1100

    Fixed spelling error in the deprecation

--- Sources/SwiftNiceties/SwiftNicetiesSet.swift
+++ Sources/SwiftNiceties/SwiftNicetiesSet.swift
@@ -8,7 +8,14 @@
 import Foundation;
 
 public extension Set where Element: Hashable {
-	@available(*, deprecated, message: "This is just a reminder - you shuld really use the .insert() function call.")
+	/**
+	 Put an element into a set.  This is **DEPRECATED** and is really only here
+	 as a reminder of the correct function call.
+
+	 - Parameter element: THe element to be inserted into the set
+	 - Returns: Void
+	 */
+	@available(*, deprecated, message: "This is just a reminder - you SHOULD really use the .insert() function call.")
 	mutating func put(_ element: Element) -> Void {
 		self.insert(element);
 	}
