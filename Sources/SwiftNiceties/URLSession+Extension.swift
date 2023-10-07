0b43d7e30d891d729403a7002f5d51e6790d50e0
Synapticloop <synapticloop@users.noreply.github.com>
2023-10-07 14:43:57 +1100

    Made URLSession public

--- Sources/SwiftNiceties/URLSession+Extension.swift
+++ Sources/SwiftNiceties/URLSession+Extension.swift
@@ -20,7 +20,7 @@ extension URLSession {
 	///
 	/// - Parameter url: The URL to retrieve the data from
 	/// - Returns: The regular data, urlresponse, and error
-	func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+	public func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
 		var data: Data?
 		var response: URLResponse?
 		var error: Error?
@@ -40,12 +40,13 @@ extension URLSession {
 
 		return (data, response, error)
 	}
+
 	/// Make a synchronous GET HTTP call - NOTE that this is a BLOCKING call so
 	/// you should ensure that this is not done on the main thread
 	/// 
 	/// - Parameter urlRequest: The URLRequest to retrieve
 	/// - Returns: The regular data, urlresponse, and error
-	func synchronousDataTask(withURLRequest urlRequest: URLRequest) -> (Data?, URLResponse?, Error?) {
+	public func synchronousDataTask(withURLRequest urlRequest: URLRequest) -> (Data?, URLResponse?, Error?) {
 		var data: Data?
 		var response: URLResponse?
 		var error: Error?
